0b1eed7da9fb5b00333b126ca238383509a45d96
Nacho <nacabaro@hotmail.com>
2025-07-29 23:08:45 +0200

    Increase version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 28
         targetSdk = 35
         versionCode = 1
-        versionName = "Alpha 0.1"
+        versionName = "Alpha 0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
